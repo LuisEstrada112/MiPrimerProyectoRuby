be36fd162c22107ca9b499d6c4deb01dc583db59
Jose <rengijose88@gmail.com>
2017-01-11 18:53:55 -0500

    ADD ingresar Producto

--- producto.rb
+++ producto.rb
@@ -17,9 +17,23 @@ class Producto
     gets()
   end
   def ingresar_producto(productos)
-
+    system('clear')
+    puts "INGRESO DE PRODUCTOS".center(50,'=')
+    print "\n\n\tCodigo de producto: "
+    cantidad=productos.size()+1
+    cantidad=cantidad.to_s
+    largo=0-cantidad.length
+    codigo="P000"
+    codigo[largo,2]=cantidad
+    print codigo
+    print "\n\n\tIngrese descripcion del producto: "
+    descripcion =gets.chomp
+    print "\n\n\tIngrese precio del producto: "
+    precio=gets.chomp
+    precio=precio.to_f
+    prod = Producto.new(codigo,descripcion,precio)
+    productos=productos.push(prod)
   end
-
   attr_accessor :codigo
   attr_accessor :descripcion
   attr_accessor :precio
