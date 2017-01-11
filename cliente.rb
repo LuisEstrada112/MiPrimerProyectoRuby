ad7d4ccf0dff3a2bd1116feb0931b45a8b929005
Jose <rengijose88@gmail.com>
2017-01-11 18:31:41 -0500

    ADD Mostrar Clientes

--- cliente.rb
+++ cliente.rb
@@ -4,6 +4,19 @@ class Cliente
     @nombre = nombre
     @direccion  = direccion
   end
+
+  def mostrar_clientes (clientes)
+    system('clear')
+    puts "LISTA DE CLIENTES".center(50,'=')
+    clientes.each do |clie|
+      print clie.codigo.ljust(5)
+      print '    '
+      print clie.nombre.ljust(15)
+      print '    '
+      puts clie.direccion.ljust(25)
+    end
+    gets()
+  end
   attr_accessor :codigo
   attr_accessor :nombre
   attr_accessor :direccion
