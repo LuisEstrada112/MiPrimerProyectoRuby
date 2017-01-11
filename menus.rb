b03c39a35fc59f365cb8ae338ab781b0ad1e9d42
Jose <rengijose88@gmail.com>
2017-01-11 17:19:10 -0500

    Crear menu Principal

---
+++ menus.rb
@@ -0,0 +1,24 @@
+require_relative 'cliente.rb'
+require_relative 'producto.rb'
+require_relative 'venta.rb'
+
+class Menus
+  def initialize
+  end
+
+  def menu_principal
+    system('clear')
+    puts "SISTEMA DE PRUEBA PARA RUBICK".center(50,'=')
+    print "\n\n"
+    puts "\t1.- Menu Cliente \n\t"
+    puts "\t2.- Menu Producto \n\t"
+    puts "\t3.- Menu venta \n\t"
+    puts "\t4.- Salir"
+    print '\n\n\tIngrese una opcion: '
+    opt=gets.chomp
+    opt = opt.to_i
+    return opt
+  end
+
+
+end
