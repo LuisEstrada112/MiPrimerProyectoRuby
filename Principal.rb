675a879cdd212fece46b2f81ef4f3f5d2d6b504b
Jose <rengijose88@gmail.com>
2017-01-11 18:58:03 -0500

    ADD Cuerpo Principal

---
+++ Principal.rb
@@ -0,0 +1,43 @@
+require_relative 'menus.rb'
+require_relative 'cliente.rb'
+require_relative 'producto.rb'
+require_relative 'venta.rb'
+
+
+
+
+cliente=Cliente.new('C001','Luis','Santa Anita')
+clientes = [].push(cliente)
+cliente=Cliente.new('C002','Manuel','Los Olivos')
+clientes = clientes.push(cliente)
+
+producto=Producto.new('P001','Leche',2.40)
+productos = [].push(producto)
+producto=Producto.new('P002','Paneton',18.50)
+productos = productos.push(producto)
+producto=Producto.new('P003','Canela',15.20)
+productos = productos.push(producto)
+producto=Producto.new('P004','Azucar',3.40)
+productos = productos.push(producto)
+producto=Producto.new('P005','Arroz',4.40)
+productos = productos.push(producto)
+
+
+men = Menus.new()
+opt=0
+opt_cliente=0
+opt_producto=0
+until opt==4 do
+  opt=men.menu_principal
+
+  if opt==1 then
+  elsif opt==2 then
+  elsif opt ==3 then
+    puts "Eligio venta"
+  elsif opt == 4 then
+    puts "Eligio Salir"
+  end
+end
+
+
+
