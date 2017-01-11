5986c5b42da86b468decfc6cc4bee03dde50b6aa
Jose <rengijose88@gmail.com>
2017-01-11 18:44:33 -0500

    ADD mostrar Producto

--- producto.rb
+++ producto.rb
@@ -4,6 +4,22 @@ class Producto
     @descripcion = descrip
     @precio = precio
   end
+  def mostrar_productos(productos)
+    system('clear')
+    puts "LISTA DE PRODUCTOS".center(50,'=')
+    productos.each do |prod|
+      print prod.codigo.ljust(5)
+      print '    '
+      print prod.descripcion.ljust(15)
+      print '    '
+      puts prod.precio.to_s.rjust(10)
+    end
+    gets()
+  end
+  def ingresar_producto(productos)
+
+  end
+
   attr_accessor :codigo
   attr_accessor :descripcion
   attr_accessor :precio
